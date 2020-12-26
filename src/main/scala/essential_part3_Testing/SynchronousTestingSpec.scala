86dc33cfb9752b60bfed6940bc18c0f59132b57b
RRRLuxoft <rrr.lands@gmail.com>
2020-12-27 00:07:32 +0100

    Section4: 23.Synchronous Testing

---
+++ src/main/scala/essential_part3_Testing/SynchronousTestingSpec.scala
@@ -0,0 +1,57 @@
+package essential_part3_Testing
+
+import akka.actor.Actor
+import akka.actor.ActorSystem
+import akka.actor.Props
+import akka.testkit.CallingThreadDispatcher
+import akka.testkit.TestActorRef
+import akka.testkit.TestProbe
+import org.scalatest.BeforeAndAfterAll
+import org.scalatest.WordSpecLike
+
+import scala.concurrent.duration.Duration
+
+class SynchronousTestingSpec extends WordSpecLike with BeforeAndAfterAll {
+
+  implicit val system: ActorSystem = ActorSystem("SynchronousTestingSpec")
+
+  override def afterAll(): Unit = system.terminate()
+
+  import SynchronousTestingSpec._
+  "A counter" should {
+    "synchronously increase its counter" in {
+      val counter = TestActorRef[Counter](Props[Counter]) //(system)
+      counter ! Inc
+      assert(counter.underlyingActor.count == 1)
+    }
+
+    "synchronously increase its counter at the call of the receive function" in {
+      val counter = TestActorRef[Counter](Props[Counter])
+      counter.receive(Inc)
+      assert(counter.underlyingActor.count == 1)
+    }
+
+    "work on the calling thread dispatcher" in {
+      val counter = system.actorOf(Props[Counter].withDispatcher(CallingThreadDispatcher.Id))
+      val probe = TestProbe()
+
+      probe.send(counter, Read)
+      probe.expectMsg(Duration.Zero, 0)
+    }
+  }
+}
+
+object SynchronousTestingSpec {
+  case object Inc
+  case object Read
+
+  class Counter extends Actor {
+    var count = 0
+
+    override def receive: Receive = {
+      case Inc  => count += 1
+      case Read => sender() ! count
+    }
+  }
+
+}
